dd082ecc89fa4afc1c1281b9223415e817cdecba
Christopher Thielen <cmthielen@ucdavis.edu>
2015-09-18 16:29:07 -0700

    Don't assume an individual exists.

--- commands/roles.rb
+++ commands/roles.rb
@@ -6,7 +6,7 @@ def roles_command(loginid)
   if roles_client.connected?
     p = roles_client.find_person_by_loginid(loginid)
 
-    if p.role_assignments.length == 0
+    if (p == null) || (p.role_assignments == null) || (p.role_assignments.length == 0)
       return "#{loginid} has no role assignments."
     end
 
