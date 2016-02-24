f6289af4968694d6ccd2b9e3c211c33a7a4b39f3
Mark Diez <markediez@gmail.com>
2016-02-24 12:09:32 -0800

    Provided messages for failed connection / nil assignments

--- commands/devboard.rb
+++ commands/devboard.rb
@@ -4,8 +4,9 @@ require 'net/https'
 
 # Get data from Devboard
 def devboard_command
-  uri =  URI.parse($SETTINGS["DEVBOARD_JSON"])
+  uri =  URI.parse($SETTINGS["DEVBOARD_URL"] + "/overview.json");
 
+  # Connecting to DevBoard
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true if uri.scheme == "https" 
   request = Net::HTTP::Get.new(uri.request_uri)
@@ -13,28 +14,36 @@ def devboard_command
 
   response = http.request(request)
 
-  # Proper Credentials met
+  # Retrieve assignments for each developer from DevBoard 
   message = ""
   if response.code == "200"
+    puts "Successfully connected to DevBoard."
+
     result = JSON.parse(response.body)
     open_assignments = result["open_assignments"]
-    
-    open_assignments.each do |open_assignment|
-      # Grab Developer
-      developer = "*" + open_assignment["developer"] + "*\n"
-      message += developer
-      # Grab Assignments
-      assignments = open_assignment["assignments"]
-      assignments.each do |assigment|
-        task = "_" + assigment["task_title"]
-        project = assigment["project"] + "_"
-        task_link = assigment["task_link"]
-        
-        message += task + " (" + project + ")" + "\n" + $SETTINGS["DEVBOARD_URL"] + task_link + "\n"
-      end # end assignment loop
-      message += "\n"
-    end # end open_assignments loop
+
+    if open_assignments 
+      open_assignments.each do |open_assignment|
+        # Grab Developer
+        developer = "*" + open_assignment["developer"] + "*\n"
+        message += developer
+        # Grab Assignments
+        assignments = open_assignment["assignments"]
+        assignments.each do |assigment|
+          task = "_" + assigment["task_title"]
+          project = assigment["project"] + "_"
+          task_link = assigment["task_link"]
+          
+          message += task + " (" + project + ")" + "\n" + $SETTINGS["DEVBOARD_URL"] + task_link + "\n"
+        end # end assignment loop
+        message += "\n"
+      end  # end open_assignments loop
+    else
+      message = "No assignments."
+    end # end if
+  else
+    puts "Unable to connect to DevBoard."
   end # end if
   
-  return message #redundant ?
+  return message 
 end
