b97edd316bbef2cb35de9f9df22b8366afb3f202
Mark Diez <markediez@gmail.com>
2016-09-28 15:16:06 -0700

    Remove sysaid integration

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'sysaid'               # for SysAid integration
 gem 'ruby-ldap'            # for LDAP integration
 gem 'daemons'              # for 'daemon-izing' the script
 gem 'slack-ruby-client'    # for Slack support
--- dss-chatbot.rb
+++ dss-chatbot.rb
@@ -45,31 +45,12 @@ Daemons.run_proc('dss-chatbot.rb') do
 
     logger.info "LDAP command(s) enabled."
   end
-  
+
   # Set up DEVBOARD support, if enabled
   if $SETTINGS['DEVBOARD_ENABLED']
     load $cwd + '/commands/devboard.rb'
-    
+
     logger.info "DevBoard command(s) enabled."
-  end
-
-  # Set up SysAid support, if enabled
-  if $SETTINGS['SYSAID_ENABLED']
-    require 'sysaid'
-
-    # Log into SysAid
-    begin
-      SysAid::login $SETTINGS['SYSAID_ACCOUNT'], $SETTINGS['SYSAID_USER'], $SETTINGS['SYSAID_PASSWORD'], $SETTINGS['SYSAID_URI']
-      logger.info "Logged into SysAid."
-    rescue Exception => e
-      $stderr.puts "Failed to connect to SysAid: #{e}"
-      logger.error "DSS ChatBot could not start because it failed to connect to SysAid. Exception: #{e}"
-      exit
-    end
-
-    load $cwd + '/commands/sysaid.rb'
-
-    logger.info "SysAid command(s) enabled."
   end
 
   # Set up Roles Management support, if enabled
@@ -117,7 +98,7 @@ Daemons.run_proc('dss-chatbot.rb') do
   client.on :hello do
     logger.info "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
   end
-  
+
   client.on :close do
     logger.info "Caught close signal. Attempting to restart ..."
     EM.stop
@@ -131,14 +112,6 @@ Daemons.run_proc('dss-chatbot.rb') do
 
     # Parse the received message for valid Chat Bot commands
     case data['text']
-    when /^sysaid/i then
-      if $SETTINGS['SYSAID_ENABLED']
-        client.message channel: data['channel'], text: sysaid_command(data['text'])
-      end
-    when /#[\d]+(\s|$|\z|\D)/ then # looks for #123 followed by space, end of string, or end of line
-      if $SETTINGS['SYSAID_ENABLED']
-        client.message channel: data['channel'], text: sysaid_command(data['text'])
-      end
     when /^ldap/ then
       if $SETTINGS['LDAP_ENABLED']
         client.message channel: data['channel'], text: ldap_command(data['text'])
