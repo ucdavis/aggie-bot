e9b6a6c54a708b2cfaf07b36f21143e9f17dc555
Mark Diez <markediez@gmail.com>
2016-10-11 11:59:40 -0700

    Describe a single command using '!help'

--- commands/help.rb
+++ commands/help.rb
@@ -6,20 +6,29 @@ module SlackBotCommand
     ENABLED_CHANNELS = ['ALL']
 
     def run(message)
-      availableCommands = "";
+      specificCommand = /(^!help)\s+([^\s]+)/.match(message)
+      response = specificCommand ? "" : "Here is a list of available commands:\n>>>"
       # Match the message to the first compatible command
       SlackBotCommand.constants.each do |command|
         # Get a reference of the command class
         commandClassReference = SlackBotCommand.const_get(command)
         commandClassReference::ENABLED_CHANNELS.each do |channel|
           if channel == "ALL" || channel == SlackBotCommand.getSource
-            availableCommands += "*" + commandClassReference::COMMAND + "*\n"
-            availableCommands += ">" + commandClassReference::DESCRIPTION + "\n"
+            if specificCommand
+              # Define a single command
+              if commandClassReference::REGEX.match(specificCommand[2])
+                response += "*" + commandClassReference::COMMAND + "*\n"
+                response += ">" + commandClassReference::DESCRIPTION + "\n"
+              end
+            else
+              # List every command
+              response += commandClassReference::COMMAND + "\n"
+            end
           end
         end
       end
 
-      return availableCommands
+      return response
     end # def run
   end # class Help
 end
