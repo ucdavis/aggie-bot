abec96dde0b92f2a3e4eb2da66125f2e1b60631d
Mark Diez <markediez@gmail.com>
2016-10-11 10:51:29 -0700

    Store the messageSource in a variable with a getter

--- slack-commands.rb
+++ slack-commands.rb
@@ -3,12 +3,23 @@ $cwd = Dir.getwd
 
 # Module that wraps around each command
 module SlackBotCommand
+  # SlackBotCommand Variables
+  @@messageSource
+
+  # Getter to access from new commands
+  def SlackBotCommand.getSource
+    return @@messageSource
+  end
+
   # Load all commands
   Dir[$cwd + "/commands/*.rb"].each {|file| require file }
 
   # Runs the proper command based on the message
   # It should return a string to output if a command is found
   def SlackBotCommand.run(message, messageSource)
+    # Set message source
+    @@messageSource = messageSource
+    
     # Match the message to the first compatible command
     SlackBotCommand.constants.each do |command|
       # Get a reference of the command class
