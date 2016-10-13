e564c49ed6d9d9f9beed15d738fc9b1b58484b5f
Mark Diez <markediez@gmail.com>
2016-10-13 14:14:46 -0700

    Update command example

--- commands/command.example.rb
+++ commands/command.example.rb
@@ -5,8 +5,13 @@
 #     COMMAND = "visioneers"     # Command to use
 #     DESCRIPTION = "TODO"       # Description of the command
 #
+#     # Run MUST return a string 
 #     def run(message)
-#       Code to run if the command is called
+#       if(Time.now.hour < 17)
+#         return "There are #{((Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0, 0) - Time.now) / 60).to_i} minutes of productivity remaining in the day."
+#       else
+#         return "There are no minutes of productivity remaining in the day."
+#       end
 #     end
 #   end
 # end
