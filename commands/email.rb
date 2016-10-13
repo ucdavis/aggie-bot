33e423eabc001efdc03d372c69467db4f173be3c
Mark Diez <markediez@gmail.com>
2016-10-13 12:40:20 -0700

    Accept username and user real name for email command

--- commands/email.rb
+++ commands/email.rb
@@ -3,21 +3,48 @@ require "net/http"
 module ChatBotCommand
   class Email
     TITLE = "Email"
-    REGEX = /^!email\s+(<\S+>)\s*$/
+    REGEX = /^!email\s+/
     COMMAND = "!email @user_name"
     DESCRIPTION = "Output the email address of the user"
 
+    # We use a class variable to avoid generating it everytime the command is called
+    @@users = Hash.new
+
     def run(message)
-      # at this point, user is "<@SOMEID>"
-      user = REGEX.match(message)[1]
+      # When used for the first time, generate the array of users
+      if @@users.size == 0
+        generate_user_array()
+      end
 
-      # we only need SOMEID
-      endIndex = user.index(">")
-      user = user[2...endIndex]
+      case message
+      when /^!email\s+(<\S+>)\s*$/ # query by @user
+        # at this point, user is "<@SOMEID>"
+        user = /^!email\s+(<\S+>)\s*$/.match(message)[1]
 
-      # Get user data from slack api
-      uri = URI.parse("https://slack.com/api/users.info")
-      args = {token: $SETTINGS["SLACK_API_TOKEN"], user: user}
+        # we only need SOMEID
+        endIndex = user.index(">")
+        user = user[2...endIndex]
+      when /(\S+)/ # query by "msdiez" / "Mark Diez"
+        # shift to remove !email
+        query = message.scan(/(\S+)/)
+        query.shift
+        user = ""
+        query.each do |item|
+          user = user + item[0] + " "
+        end
+
+        # remove ending whitespace
+        user.strip!
+      end
+
+      response = @@users[user] ? @@users[user] : "User does not exist"
+      return response
+    end # def run
+
+    def generate_user_array
+      # Get a list of user data from slack api
+      uri = URI.parse("https://slack.com/api/users.list")
+      args = {token: $SETTINGS["SLACK_API_TOKEN"]}
       uri.query = URI.encode_www_form(args)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
@@ -26,10 +53,20 @@ module ChatBotCommand
 
       if response.code == "200"
         result = JSON.parse(response.body)
-        return result["user"]["profile"]["email"]
+        users = result["members"]
+        users.each do |user|
+          username = user["name"]
+          userId = user["id"]
+          userFullName = user["real_name"]
+          userEmail = user["profile"]["email"]
+
+          @@users[userId] = userEmail
+          @@users[username] = userEmail
+          @@users[userFullName] = userEmail
+        end
       else
         return "Could not connect to Slack API due to #{response.code}"
       end
-    end
-  end
-end
+    end # def generate_user_array
+  end # class Email
+end # module ChatBotCommand
