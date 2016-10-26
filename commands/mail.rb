10a02aefefea61ed9a2838fc058df26a2d94454d
Mark Diez <markediez@gmail.com>
2016-10-26 14:53:52 -0700

    Get information using mechanize

--- commands/mail.rb
+++ commands/mail.rb
@@ -14,6 +14,35 @@ module ChatBotCommand
       return "TODO"
     end
 
+    # Retrieves mail forwarding infomration from iet.ucdavis.edu
+    # @param login_id - Kerberos login id to query
+    # @return - hash containing the information, else a string
+    def get_information login_id
+      # Success! Load the page and use the CAS credentials
+      agent = Mechanize.new
+
+      # Certain web servers check the user agent string for compatibility
+      agent.user_agent_alias = 'Mac Firefox'
+
+      # Run Mail ID lookup
+      page = agent.get('https://iet.ucdavis.edu/itprovider')
+
+      # Find form TODO: try to see if we can find it dynamically?
+      mail_id_form = page.forms[2]
+
+      # Fill form
+      mail_id_form.fields[0].value = login_id
+
+      # Submit Form
+      page = agent.submit(form, form.buttons.first)
+
+      unless page.code == "200"
+        return "Could not complete the task due to " + page.code
+      end
+
+      return page.body
+    end
+
     # Essential to make commands a singleton
     @@instance = Mail.new
     def self.get_instance
