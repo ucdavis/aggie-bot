62562b9c3461d295ccf4e71dc9c1c9e2e83b4d00
Mark Diez <markediez@gmail.com>
2016-10-26 12:12:58 -0700

    Consider an array of information per iamId

--- commands/iam.rb
+++ commands/iam.rb
@@ -47,31 +47,31 @@ module ChatBotCommand
 
       # Get ids, names and affiliations
       basic_info = get_from_api("api/iam/people/search", {"iamId" => iam_id})
-      result["basic_info"] = basic_info.empty? ? [] : basic_info[0]
+      result["basic_info"] = basic_info.empty? ? [] : basic_info
 
       # Get email, postaladdress, phone numbers
       contact_info = get_from_api("api/iam/people/contactinfo/search", {"iamId" => iam_id})
-      result["contact_info"] = contact_info.empty? ? [] : contact_info[0]
+      result["contact_info"] = contact_info.empty? ? [] : contact_info
 
       # Get kerberos loginid
       kerberos_info = get_from_api("api/iam/people/prikerbacct/search", {"iamId" => iam_id})
-      result["kerberos_info"] = kerberos_info.empty? ? [] : kerberos_info[0]
+      result["kerberos_info"] = kerberos_info.empty? ? [] : kerberos_info
 
       # Get PPS department, title, and position type
       pps_info = get_from_api("api/iam/associations/pps/search", {"iamId" => iam_id})
-      result["pps_info"] = pps_info.empty? ? [] : pps_info[0]
+      result["pps_info"] = pps_info.empty? ? [] : pps_info
 
       # Get ODR department and title
       odr_info = get_from_api("api/iam/associations/odr/search", {"iamId" => iam_id})
-      result["odr_info"] = odr_info.empty? ? [] : odr_info[0]
+      result["odr_info"] = odr_info.empty? ? [] : odr_info
 
       # Get HS employee information
       hs_info = get_from_api("api/iam/associations/hs/search", {"iamId" => iam_id})
-      result["hs_info"] = hs_info.empty? ? [] : hs_info[0]
+      result["hs_info"] = hs_info.empty? ? [] : hs_info
 
       # Get student information
       student_info = get_from_api("api/iam/associations/sis/search", {"iamId" => iam_id})
-      result["student_info"] = student_info.empty? ? [] : student_info[0]
+      result["student_info"] = student_info.empty? ? [] : student_info
 
       return result
     end
@@ -125,76 +125,113 @@ module ChatBotCommand
     # Formats the data from IAM API to a prettier format
     # @param data - the hash obtained from gather_data
     def format_data data
-      # Format name with flags
-      name = data["basic_info"]["dFullName"]
+      # Store all formatted data in this array
+      formatted_data = []
 
-      flags = []
-      if data["basic_info"]["isEmployee"]
-        flags.push "employee"
+      # Format name with flags
+      # Name Mark Diez (student, employee, staff)
+      if !data["basic_info"].empty?
+        data["basic_info"].each do |info|
+          name = "*Name* " + info["dFullName"]
+
+          flags = []
+          if info["isEmployee"]
+            flags.push "employee"
+          end
+          if info["isHSEmployee"]
+            flags.push "hs employee"
+          end
+          if info["isFaculty"]
+            flags.push "faculty"
+          end
+          if info["isStudent"]
+            flags.push "student"
+          end
+          if info["isStaff"]
+            flags.push "staff"
+          end
+          if info["isExternal"]
+            flags.push "external"
+          end
+          flags = " _(" + flags.join(", ") + ")_"
+          name += flags
+
+          formatted_data.push name
+        end
       end
-      if data["basic_info"]["isHSEmployee"]
-        flags.push "hs employee"
-      end
-      if data["basic_info"]["isFaculty"]
-        flags.push "faculty"
-      end
-      if data["basic_info"]["isStudent"]
-        flags.push "student"
-      end
-      if data["basic_info"]["isStaff"]
-        flags.push "staff"
-      end
-      if data["basic_info"]["isExternal"]
-        flags.push "external"
-      end
-      flags = " _(" + flags.join(", ") + ")_"
-      name += flags
 
       # Format Kerberos information
-      loginid = data["kerberos_info"].empty? ? "Not Listed" : data["kerberos_info"]["userId"]
+      # Login ID msdiez, anotherid, ...
+      if !data["kerberos_info"].empty?
+          ids = []
+        data["kerberos_info"].each do |info|
+          id = info["userId"] == nil ? "Not Listed" : info["userId"]
+          ids.push id
+        end
+
+        formatted_data.push "*Login ID* " + ids.join(", ")
+      else
+        login_id = "*Login ID* Not Listed"
+        formatted_data.push login_id
+      end
+
 
       # Format contact information
-      email = data["contact_info"].empty? ? "Not Listed" : data["contact_info"]["email"]
-      office = "Not Listed"
-      office = data["contact_info"]["addrStreet"] unless data["contact_info"].empty? || data["contact_info"]["addrStreet"] == nil
-
-      # Store affiliations in this array
-      affiliations = []
+      # E-mail my@email.com, my@otheremail.com
+      # Office kerr 186, social science 133, ...
+      if !data["contact_info"].empty?
+        email = []
+        office = []
+        data["contact_info"].each do |info|
+          email.push info["email"] unless info["email"] == nil
+          office.push info["addrStreet"] unless info["addrStreet"] == nil
+        end
+        formatted_data.push "*E-mail* " + email.join(", ")
+        formatted_data.push "*Office* " + office.join(", ")
+      else
+        formatted_data.push "*E-mail* Not Listed"
+        formatted_data.push "*Office* Not Listed"
+      end
 
       # Format ODR information
+      # ODR Affiliation DSSIT: STD4 (Casual)
       if !data["odr_info"].empty?
-        odr = "*ODR Affiliation* "
-        odr += data["odr_info"]["deptDisplayName"] + ": " unless data["odr_info"]["deptDisplayName"] == nil
-        odr += data["odr_info"]["titleDisplayName"] unless data["odr_info"]["titleDisplayName"] == nil
-        affiliations.push odr
+        data["odr_info"].each do |info|
+          odr = "*ODR Affiliation* "
+          odr += info["deptDisplayName"] + ": " unless info["deptDisplayName"] == nil
+          odr += info["titleDisplayName"] unless info["titleDisplayName"] == nil
+
+          formatted_data.push odr
+        end
       end
 
       # Format PPS information
+      # PPS Affiliation DSSIT: STD4
       if !data["pps_info"].empty?
-        pps = "*PPS Affiliation* "
-        pps += data["pps_info"]["deptDisplayName"] + ": " unless data["pps_info"]["deptDisplayName"] == nil
-        pps += data["pps_info"]["titleDisplayName"]unless data["pps_info"]["titleDisplayName"] == nil
-        pps += " (" + data["pps_info"]["positionType"] + ")" unless data["pps_info"]["positionType"] == nil
-        affiliations.push pps
+        data["pps_info"].each do |info|
+          pps = "*PPS Affiliation* "
+          pps += info["deptDisplayName"] + ": " unless info["deptDisplayName"] == nil
+          pps += info["titleDisplayName"]unless info["titleDisplayName"] == nil
+          pps += " (" + info["positionType"] + ")" unless info["positionType"] == nil
+
+          formatted_data.push pps
+        end
       end
 
       # Format student information
+      # Student Affiliation Computer Science (Undergraduate, Junior)
       if !data["student_info"].empty?
-        student = "*Student Affiliation* "
-        student += data["student_info"]["majorName"] + " ("
-        student += data["student_info"]["levelName"].scan(/\S+/)[0] # Only grab the first word
-        student +=  ", " + data["student_info"]["className"] + ")"
-        affiliations.push student
+        data["student_info"].each do |info|
+          student = "*Student Affiliation* "
+          student += info["majorName"] + " ("
+          student += info["levelName"].scan(/\S+/)[0] # Only grab the first word
+          student +=  ", " + info["className"] + ")"
+
+          formatted_data.push student
+        end
       end
 
-      # Format response
-      response = "*Name* #{name}\n"
-      response += "*Login* #{loginid}\n"
-      response += "*E-mail* #{email}\n"
-      response += "*Office* #{office}\n"
-      response += affiliations.empty? ? "" : affiliations.join("\n")
-
-      return response
+      return formatted_data.join("\n")
     end
 
     # Returns an object containing the result from an api call, else an error message
